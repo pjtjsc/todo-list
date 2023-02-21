27c7946b2007e0c27be6796e279701278f5d6567
pjtjsc <pinjhutuan@gmail.com>
2023-02-21 21:50:46 +0800

    connecting problem

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const express = require('express')
+const mongoose = require('mongoose') // 載入 mongoose
+
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 const app = express()
+mongoose.connect(process.env.MONGODB_URI) // 設定連線到 mongoDB
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+mongoose.connect()
 
 app.get('/' , (req,res)=>{
   res.send('hello world!')
